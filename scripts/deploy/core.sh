42a713ed18265bdfef59216848683830ca9186a4
Alexey Derlaft <derlafff@ya.ru>
2016-07-23 03:25:56 +0300

    Fix vendor shiet

--- scripts/deploy/core.sh
+++ scripts/deploy/core.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find vendor -type d -name views | while read dir; do
+find vendor src -type d -name views | while read dir; do
   install -d "$dir" "$1/$dir"
   cp -a "$dir"/* "$1/$dir"
 done
