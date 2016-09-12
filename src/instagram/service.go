6dc781c308ba4f9486265924b631e1a538fb0387
Alexey Derlaft <derlafff@ya.ru>
2016-09-12 10:37:49 +0300

    Fix api

--- src/instagram/service.go
+++ src/instagram/service.go
@@ -14,9 +14,9 @@ import (
 // Global constants
 const (
 	URL           = "https://i.instagram.com/api/v1"
-	Version       = "9.2.5"
+	Version       = "9.0.0"
 	UserAgent     = "Instagram " + Version + " Android (23/6.0.1; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)"
-	SigKey        = "313402966dbce954860042c7d18f898a4290c833ced8c1913866fdb89d8a9562"
+	SigKey        = "5519c493fc0e2d56355621cf0d4485611869067c7e26f89e946070e3c526df7d"
 	SigKeyVersion = "4"
 )
 
@@ -28,18 +28,16 @@ func generateUUID(t bool) string {
 	return u
 }
 
-// generate android device ID
-func generateDeviceID(salt string) string {
-	hash := md5.New()
-	hash.Write([]byte(salt))
-	hash.Write([]byte{1, 2, 3})
-	return "android-" + hex.EncodeToString(hash.Sum(nil))[:16]
-}
-
-// generate request signature
 func generateSignature(data []byte) string {
 	h := hmac.New(sha256.New, []byte(SigKey))
 	h.Write(data)
 	hash := hex.EncodeToString(h.Sum(nil))
 	return "ig_sig_key_version=" + SigKeyVersion + "&signed_body=" + hash + "." + url.QueryEscape(string(data))
+}
+
+func generateDeviceID(salt string) string {
+	hash := md5.New()
+	hash.Write([]byte(salt))
+	hash.Write([]byte{1, 2, 3})
+	return "android-" + hex.EncodeToString(hash.Sum(nil))[:16]
 }
