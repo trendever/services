f4058f1a3943035a23de253fec9e0dc9e667fb15
Alexey Derlaft <derlafff@ya.ru>
2016-09-12 10:37:12 +0300

    Revert "Revert "Some more refactoring before switching to branch""
    This reverts commit 13487ea13628f9139008db2699c0987f60aef93f.

--- src/instagram/pool.go
+++ src/instagram/pool.go
@@ -6,19 +6,31 @@ import (
 	"time"
 )
 
+// Pool allows to use multiple accounts easily.
+// Instagram fastly bans when it comes to repititive and common requests
+// Create a pool, add joined APIs to it
+// Every time you use GetFree() you will get a Client instance
+// Every client is returned delayed at some random time between TimeoutMin and TimeoutMax
+// If there is a problem with client (it's banned for instance), it is delayed with ReloginTimeout
+
+// Pool defines thread-safe account pool
 type Pool struct {
 	storage  chan *Instagram
-	stoppers map[*Instagram]chan bool
 	settings *PoolSettings
-	sync.Mutex
+
+	stoppers map[*Instagram]chan bool
+	mutex    sync.Mutex // mutex only for map access
 }
 
+// PoolSettings defines settings for pool to be created
+// Timeouts-only for now
 type PoolSettings struct {
 	ReloginTimeout int // Milliseconds
 	TimeoutMin     int
 	TimeoutMax     int
 }
 
+// NewPool creates a new pool
 func NewPool(settings *PoolSettings) *Pool {
 	return &Pool{
 		storage:  make(chan *Instagram),
@@ -27,29 +39,33 @@ func NewPool(settings *PoolSettings) *Pool {
 	}
 }
 
+// Add connection to the pool
 func (p *Pool) Add(api *Instagram) {
 	stop := make(chan bool)
 
-	p.Lock()
+	p.mutex.Lock()
 	p.stoppers[api] = stop
-	p.Unlock()
+	p.mutex.Unlock()
 
 	go p.poolWorker(api, stop)
 }
 
+// Remove only one connection from the pool
 func (p *Pool) Remove(api *Instagram) {
-	p.Lock()
+	p.mutex.Lock()
 	p.stoppers[api] <- true
 	delete(p.stoppers, api)
-	p.Unlock()
+	p.mutex.Unlock()
 }
 
+// RemoveAll deletes all connections from the pool
 func (p *Pool) RemoveAll() {
 	for api := range p.stoppers {
 		p.Remove(api)
 	}
 }
 
+// main loop started for each connection
 func (p *Pool) poolWorker(api *Instagram, stop <-chan bool) {
 	for {
 		select {
@@ -68,6 +84,8 @@ func (p *Pool) poolWorker(api *Instagram, stop <-chan bool) {
 	}
 }
 
+// GetFree returns first freed Connection
+// First available client is waited
 func (p *Pool) GetFree() *Instagram {
 	api := <-p.storage
 	return api
