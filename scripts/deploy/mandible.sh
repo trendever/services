03e188eb40ddc8e8a5afda67ea753a9fa1d72dc6
Alexey Derlaft <derlafff@ya.ru>
2016-10-01 14:27:17 +0300

    mandible: try to fix deploy script

--- scripts/deploy/mandible.sh
+++ scripts/deploy/mandible.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 cat > "$1/onbuild.sh" << EOF
+  apk add ca-certificates
+  update-ca-certificates
   cat > /etc/apk/repositories << APK
 http://dl-cdn.alpinelinux.org/alpine/edge/main
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 APK
   apk update
-  apk upgrade
   ln -sv /usr/bin/gm /usr/bin/convert
   apk add graphicsmagick optipng libjpeg-turbo-utils exiftool
   rm -rf /var/cache/apk/*
