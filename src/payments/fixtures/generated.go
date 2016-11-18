ebc48e05ee48ca6da5c94245ee0b597efa853af1
Alexey Derlaft <derlafff@ya.ru>
2016-11-18 11:18:12 +0300

    payments: not enought dev resources to support tests

--- src/payments/fixtures/generated.go
+++
@@ -1,5 +0,0 @@
-package fixtures
-
-//go:generate mockgen -package=fixtures -destination=mock_repo.go payments/models Repo
-//go:generate mockgen -package=fixtures -destination=mock_gw.go payments/models Gateway
-//go:generate mockgen -package=fixtures -destination=mock_chat.go payments/api ChatNotifier
