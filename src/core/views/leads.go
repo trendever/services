e6b7e16293b2385665acd969b252230d4b96d28d
Anton <anlopan@gmail.com>
2017-02-13 00:28:48 +0300

    Core: change instaname source

--- src/core/views/leads.go
+++ src/core/views/leads.go
@@ -64,7 +64,7 @@ func (s leadServer) CreateLead(ctx context.Context, protoLead *core.Lead) (*core
 		}
 
 		//Also match if shop name supplied in comment
-		if strings.Contains(comment_prepared, fmt.Sprintf("%s", product.Shop.Supplier.InstagramUsername)) {
+		if strings.Contains(comment_prepared, fmt.Sprintf("%s", product.Shop.InstagramUsername)) {
 			is_comment_matched = true
 		}
 
