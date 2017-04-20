97df6f87b663c695e2aedeac49efb9c0c7883193
Anton <anlopan@gmail.com>
2017-04-20 21:58:44 +0300

    Chat: fix message counters

--- src/chat/models/conversation.go
+++ src/chat/models/conversation.go
@@ -408,8 +408,11 @@ func (c *conversationRepositoryImpl) GetUnread(ids []uint64, userID uint64) (map
 	rows, err := c.db.Model(&Message{}).
 		Select("count(messages.id), messages.conversation_id").
 		Joins("LEFT JOIN members ON (members.conversation_id = messages.conversation_id AND members.user_id = ?)", userID).
+		Joins("LEFT JOIN message_parts ON (message_parts.message_id = messages.id)").
 		Where("(messages.id > members.last_message_id OR members.last_message_id IS NULL)").
 		Where("messages.conversation_id in (?)", ids).
+		Where("messages.member_id != members.id").
+		Where("message_parts.mime_type != 'json/status'").
 		Group("messages.conversation_id").
 		Rows()
 	if err != nil {
