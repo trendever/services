7d35af30be948be18963fde9494b459707c019dc
betrok <betrok@ya.ru>
2017-10-03 15:17:28 +0300

    move common libs to submodule

--- src/common/metrics/helpers.go
+++
@@ -1,8 +0,0 @@
-package metrics
-
-import "time"
-
-//ToMs converts time.Duration to milliseconds
-func ToMs(d time.Duration) float64 {
-	return float64(d.Nanoseconds()/int64(time.Millisecond)) + float64(d.Nanoseconds()%int64(time.Millisecond))*1e-9
-}
